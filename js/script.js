6412fdadb99dc40e4c6d04b99dd95f18ba33536f
Marta Nara <marta.kos1230@gmail.com>
2021-10-09 18:44:38 +0200

    Finalize event handler logic, fix google fonts link in scss

--- js/script.js
+++ js/script.js
@@ -3,26 +3,22 @@
 const titleClickHandler = function(event){
     event.preventDefault();
     const clickedElement = this;
-    console.log('Link was clicked!');
   
     /* [DONE] remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
-        activeLink.classList.remove('active');
+    activeLink.classList.remove('active');
     }
   
     /* [DONE] add class 'active' to the clicked link */
 
     clickedElement.classList.add('active')
-
-    console.log('clickedElement:', clickedElement);
-    console.log('clickedElement (with plus): ' + clickedElement);
   
     /* [DONE] remove class 'active' from all articles */
 
-    const activeArticles = document.querySelectorAll('.post .active');
+    const activeArticles = document.querySelectorAll('.posts .active');
 
     for(let activeArticle of activeArticles){
         activeArticle.classList.remove('active');
@@ -30,18 +26,21 @@ const titleClickHandler = function(event){
   
     /* get 'href' attribute from the clicked link */
 
-    const clickedArticle = clickedElement.getAttribute('href');
-    console.log('clickedArticle:', clickedArticle);
+    const articleSelector = clickedElement.getAttribute('href');
   
     /* find the correct article using the selector (value of 'href' attribute) */
+
+    const targetArticle = document.querySelector(articleSelector)
   
     /* add class 'active' to the correct article */
+
+    targetArticle.classList.add('active')
 }
   
 const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
-    link.addEventListener('click', titleClickHandler);
+  link.addEventListener('click', titleClickHandler);
 }
 
 /*
