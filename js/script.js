6d7935953855967f6c8ef47ead9205c64aead627
Marta Nara <marta.kos1230@gmail.com>
2021-10-09 15:19:36 +0200

    Add js for adding class 'active' to clicked links

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ const titleClickHandler = function(event){
         activeLink.classList.remove('active');
     }
   
-    /* [IN PROGRESS] add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
 
     clickedElement.classList.add('active')
 
