7b11c71a98b0bd2efcce309770c3ed01ebbce77e
Marta Nara <marta.kos1230@gmail.com>
2021-10-13 19:39:53 +0200

    Add eslint, stylelint, editorconfig to the project and fix all stylistic errors

--- js/script.js
+++ js/script.js
@@ -1,59 +1,59 @@
 {
 
-'use strict';
+  'use strict';
 
-const titleClickHandler = function(event){
+  const titleClickHandler = function(event){
     event.preventDefault();
     const clickedElement = this;
-  
+
     /* remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
-    activeLink.classList.remove('active');
+      activeLink.classList.remove('active');
     }
-  
+
     /* add class 'active' to the clicked link */
 
-    clickedElement.classList.add('active')
-  
+    clickedElement.classList.add('active');
+
     /* remove class 'active' from all articles */
 
     const activeArticles = document.querySelectorAll('.posts .active');
 
     for(let activeArticle of activeArticles){
-        activeArticle.classList.remove('active');
+      activeArticle.classList.remove('active');
     }
-  
+
     /* get 'href' attribute from the clicked link */
 
     const articleSelector = clickedElement.getAttribute('href');
-  
+
     /* find the correct article using the selector (value of 'href' attribute) */
 
-    const targetArticle = document.querySelector(articleSelector)
-  
+    const targetArticle = document.querySelector(articleSelector);
+
     /* add class 'active' to the correct article */
 
-    targetArticle.classList.add('active')
-}
+    targetArticle.classList.add('active');
+  };
 
-const optArticleSelector = '.post',
+  const optArticleSelector = '.post',
     optTitleSelector = '.post-title',
     optTitleListSelector = '.titles';
 
-const generateTitleLinks = function(){
+  const generateTitleLinks = function(){
 
     /* remove contents of titleList */
 
     const titleList = document.querySelector(optTitleListSelector);
 
     const clearList = function(list) {
-        list.innerHTML = '';
-    }
+      list.innerHTML = '';
+    };
 
-    clearList(titleList)
+    clearList(titleList);
 
     const articles = document.querySelectorAll(optArticleSelector);
 
@@ -63,34 +63,33 @@ const generateTitleLinks = function(){
 
     for(let article of articles){
 
-        /* get the article id */
+      /* get the article id */
 
-        const articleID = article.getAttribute('id')
+      const articleID = article.getAttribute('id');
 
-        /* find the title element */
-        /* get the title from the title element */
+      /* find the title element */
+      /* get the title from the title element */
 
-        const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
 
-        /* create HTML of the link */
+      /* create HTML of the link */
 
-        const linkHTML = '<li><a href="#' + articleID + '"><span>' + articleTitle + '</span></a></li>';
-        
-        html = html + linkHTML;
+      const linkHTML = '<li><a href="#' + articleID + '"><span>' + articleTitle + '</span></a></li>';
+
+      html = html + linkHTML;
     }
 
     /* insert link into titleList */
-    
+
     titleList.insertAdjacentHTML('beforeend', html);
 
     const links = document.querySelectorAll('.titles a');
 
     for(let link of links){
-        link.addEventListener('click', titleClickHandler);
+      link.addEventListener('click', titleClickHandler);
     }
+  };
+
+  generateTitleLinks();
+
 }
-
-generateTitleLinks()
-
-}
-
